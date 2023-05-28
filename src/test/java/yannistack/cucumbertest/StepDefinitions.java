25e90b2e7921dd009fea409be195bcd92ba0a998
Yannis Tack <yannis.tack@hotmail.com>
2023-05-28 22:42:11 +0200

    Added new page functions + related stepdefinitions

--- src/test/java/yannistack/cucumbertest/StepDefinitions.java
+++ src/test/java/yannistack/cucumbertest/StepDefinitions.java
@@ -16,6 +16,20 @@ public class StepDefinitions {
     	System.out.println("StepDef - HOMEPAGE");
     	page = new HomePage(DriverManager.getEDriver());
     }
+    
+    @Given("the item called {string} is not yet added")
+    public void removeItemIfExists(String s) {
+    	System.out.println("Item check: " + s);
+    	if (page.isListItemVisible(s)) {
+    		page.deleteTodoItem(s);
+    	}
+    }
+    
+    @Given("there are no (todo) items")
+    public void clearAllItems() {
+    	System.out.println("Clearing all items");
+    	page.deleteAllTodoItems();
+    }
 
     @When("^I\\s(?:add|added)\\sa\\snew\\stodo\\sitem\\scalled\\s\"(.+)\"$")
     public void addTodoItem(String itemName) {
@@ -32,12 +46,22 @@ public class StepDefinitions {
     @When("^I\\sdelete\\sthe\\stodo\\sitem\\scalled\\s\"(.+)\"$")
     public void deleteTodoItem(String itemName) {
     	System.out.println("Deleting todo item '" + itemName + "'");
+    	page.deleteTodoItem(itemName);
     }
 
     @Then("^the\\s(?:todo)?\\s?item\\scalled\\s\\\"(.+)\\\"\\sis\\svisible$")
-    public void theScenarioPasses(String itemName) {
-    	System.out.println("StepDef - ASSERT");
+    public void theItemIsVisible(String itemName) {
+    	System.out.println("Validating item " + itemName + " is visible");
     	assertTrue(page.isListItemVisible(itemName));
     }
-
+    
+    @Then("the item called {string} is not visible")
+    public void theItemIsInvisible(String itemName) {
+    	System.out.println("Validating item " + itemName + " is not visible");
+    }
+    
+    @Then("{int} (todo) items are visible")
+    public void numberOfItemsAreVisible(int itemCount) {
+    	System.out.println("Validating itemlist has " + itemCount + " todo items");
+    }
 }
